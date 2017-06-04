1a3fc20aec2eee09e5983ac17aff73e40dbe2c0e
Stephen <stephengroat@users.noreply.github.com>
2017-06-04 08:11:32 -0700

    Update callflowdiff.py

--- callflowdiff.py
+++ callflowdiff.py
@@ -1,6 +1,6 @@
-import llvmlite
+import llvmlite.binding as llvm
 
 mod = llvm.parse_assembly("curl-7.54.0/src/.libs/curl.0.4.opt.bc")
 mod.verify()
 for func in mod.functions:
-  print llvmlite.binding.get_function_cfg(func)
+  print llvm.get_function_cfg(func)
