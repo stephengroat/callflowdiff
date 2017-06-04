d7bafb210b820452752ee9cf5b8b1667a549358c
Stephen <stephengroat@users.noreply.github.com>
2017-06-04 08:49:26 -0700

    Update callflowdiff.py

--- callflowdiff.py
+++ callflowdiff.py
@@ -1,6 +1,12 @@
 import llvmlite.binding as llvm
+import pygraphviz
+import networkx
 
 mod = llvm.parse_bitcode(open("curl-7.54.0/src/.libs/curl.0.4.opt.bc", 'r').read())
 mod.verify()
+graph = ""
 for func in mod.functions:
-  print llvm.get_function_cfg(func)
+  graph += llvm.get_function_cfg(func)
+graph2 = pygraphviz.AGraph(graph)
+graph3 = networkx.Graph(graph2)
+networkx.draw(graph3)
